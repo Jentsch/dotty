77627ab5f1604d32fb7ec0412989fadd05547e70
Jentsch <d@jentsch.berlin>
2021-06-28 08:37:09 +0200

    Expand examples

--- tests/pos/namedPatternMatching.scala
+++ tests/pos/namedPatternMatching.scala
@@ -1,15 +1,34 @@
 package patterns
 
-case class User(name: String, age: Int, city: String)
+case class Age(years: Int)
 
-val user = User(name = "Anna", age = 10, city = "Berlin")
+case class User(name: String, age: Age, city: String)
+
+val user = User(name = "Anna", age = Age(10), city = "Berlin")
 
 val annasCity = user match
   case User(name = "Tom", city = city) => ???
   case User(city = c, name = s"Ann$_") => c
   case User(name = guy @ ("Guy" | "guy")) => ???
 
-@main
-def main(): Unit = {
-  println(annasCity)
+// nested patterns
+val User(name = name, age = Age(years = years)) = user
+
+// partial funtion
+val maybeTom = Some(user).collect {
+  case u @ User(name = "Tom") => u
 }
+
+val berlinerNames = for
+  case User(city = "Berlin", name = name) <- List(user)
+yield name
+
+@main
+def main(): Unit =
+  println(annasCity)
+  println(name)
+  println(years)
+
+  println(maybeTom)
+  println(berlinerNames)
+
